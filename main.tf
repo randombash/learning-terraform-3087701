1127a4830a7e76e3433dda841e056664ca0cfd5e
randombash <42682464+randombash@users.noreply.github.com>
2024-11-04 11:54:21 +0100

    -x

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ data "aws_ami" "app_ami" {
     name   = "name"
     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
-x
+:west
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
