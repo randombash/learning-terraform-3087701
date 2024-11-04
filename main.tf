b23eec8774cfe1fc7cd9c1b40617224fc48bf720
randombash <42682464+randombash@users.noreply.github.com>
2024-11-04 11:30:59 +0100

    add vpc

--- main.tf
+++ main.tf
@@ -19,6 +19,23 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev":
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  subnet_id = blog_vpc.public_subnets.0
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
@@ -34,7 +51,7 @@ module "blog_sg" {
   version = "5.2.0"
   name ="blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
